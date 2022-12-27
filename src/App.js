883fd1b1ee12b6457ba0f420f027df40016c1465
MahsaMoadab <mahsa.moadab.s@gmail.com>
2022-12-27 18:29:29 +0330

    [src] add components

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    async function getApi() {
+        const response = await fetch('https://random-d.uk/api/random');
+        return (await response.json());
+    }
+
+    useEffect(() => {
+        getApi()
+            .then((data) => {
+                setData(data);
+                setLoading(false);
+            })
+    }, [])
+
+
+    return (
+        <>
+            {!loading && <div className="App">
+                <img src={data.url} alt={''} />
+                <p>{data.message}</p>
+            </div>}
+        </>
+    );
+}
+
+export default App;
